b5eb18b424c94b7735687d352841e4632af8616e
Arvind Singh Rawat <arrawat@deloitte.com>
2024-07-05 14:04:20 +0530

    Added support for configuration

--- src/main/java/com/asr/example/vert/x/demo/AppVerticle.java
+++ src/main/java/com/asr/example/vert/x/demo/AppVerticle.java
@@ -1,10 +1,16 @@
 package com.asr.example.vert.x.demo;
 
+import com.asr.example.vert.x.demo.config.BaseConfiguration;
 import com.asr.example.vert.x.demo.route.HelloWorldRoute;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 
 public class AppVerticle extends AbstractVerticle {
@@ -17,20 +23,37 @@ public class AppVerticle extends AbstractVerticle {
   public void start(Promise<Void> startPromise) {
 
     Router router = Router.router(vertx);
-    HelloWorldRoute.attach(router);
 
-    vertx
-      .createHttpServer()
-      .requestHandler(router)
-      .exceptionHandler(error -> LOGGER.error("HTTP server error: ", error))
-      .listen(APP_PORT, http -> {
-          if (http.succeeded()) {
-            startPromise.complete();
-            LOGGER.info("HTTP server started on port " + APP_PORT);
-          } else {
-            startPromise.fail(http.cause());
-          }
+    // Configuration setup
+    setUpConfig()
+      .onComplete(json -> {
+          BaseConfiguration result = json.result().mapTo(BaseConfiguration.class);
+          HelloWorldRoute.attach(router, result);
+          vertx
+            .createHttpServer()
+            .requestHandler(router)
+            .exceptionHandler(error -> LOGGER.error("HTTP server error: ", error))
+            .listen(APP_PORT, http -> {
+                if (http.succeeded()) {
+                  startPromise.complete();
+                  LOGGER.info("HTTP server started on port " + APP_PORT);
+                } else {
+                  startPromise.fail(http.cause());
+                }
+              }
+            );
         }
       );
+
+
+  }
+
+  private Future<JsonObject> setUpConfig() {
+    ConfigStoreOptions configStoreOptions = new ConfigStoreOptions()
+      .setFormat("properties")
+      .setType("file")
+      .setConfig(new JsonObject().put("path", "application.properties").put("raw-data", true));
+    ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(configStoreOptions);
+    return ConfigRetriever.create(vertx, options).getConfig();
   }
 }
--- src/main/java/com/asr/example/vert/x/demo/route/HelloWorldRoute.java
+++ src/main/java/com/asr/example/vert/x/demo/route/HelloWorldRoute.java
@@ -1,5 +1,6 @@
 package com.asr.example.vert.x.demo.route;
 
+import com.asr.example.vert.x.demo.config.BaseConfiguration;
 import com.asr.example.vert.x.demo.dto.http.response.SayingDto;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
@@ -10,17 +11,20 @@ public class HelloWorldRoute {
 
   private static final AtomicLong COUNT = new AtomicLong(0L);
 
-  public static void attach(Router web) {
+  public static void attach(Router web, BaseConfiguration result) {
     web
       .route("/hello-world")
-      .handler(routingContext -> {
-        routingContext.response()
-          .putHeader("Content-Type", "application/json")
-          .end(JsonObject.mapFrom(createResponse()).encode());
-      });
+      .handler(routingContext -> routingContext
+        .response()
+        .putHeader("Content-Type", "application/json")
+        .end(
+          JsonObject
+            .mapFrom(createResponse(result.getTemplate(), result.getDefaultName()))
+            .encode()
+        ));
   }
 
-  private static SayingDto createResponse() {
-    return new SayingDto(COUNT.getAndIncrement(), "Hello Stranger!");
+  private static SayingDto createResponse(String template, String defaultName) {
+    return new SayingDto(COUNT.getAndIncrement(), template.formatted(defaultName));
   }
 }
